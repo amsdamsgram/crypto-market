35555db73522b09a346944ba5c32f9d3bb0bd258
Damien Soulard <1591702+amsdamsgram@users.noreply.github.com>
2019-03-02 12:01:47 +0100

    chore: set up jest (#3)

--- App.test.tsx
+++ App.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "react-native";
 
-import App from "../App";
+import App from "./App";
 
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
